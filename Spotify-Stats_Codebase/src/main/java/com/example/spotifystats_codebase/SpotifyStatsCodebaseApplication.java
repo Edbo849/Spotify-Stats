1f6e3e5ff52ed1848eb9a157da4664be8f0383c4
Edbo849 <edborthwick84@gmail.com>
2024-06-06 18:26:51 +0100

    Setup Springboot

---
+++ Spotify-Stats_Codebase/src/main/java/com/example/spotifystats_codebase/SpotifyStatsCodebaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotifystats_codebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyStatsCodebaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyStatsCodebaseApplication.class, args);
+    }
+
+}
---
+++ Spotify-Stats_Codebase/src/test/java/com/example/spotifystats_codebase/SpotifyStatsCodebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotifystats_codebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyStatsCodebaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
