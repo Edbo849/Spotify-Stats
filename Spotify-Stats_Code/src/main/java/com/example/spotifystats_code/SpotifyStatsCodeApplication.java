a6868748cc61baf2902ad1ab50aa46f7962900d9
Edbo849 <edborthwick84@gmail.com>
2024-06-07 18:01:30 +0100

    Create Spring Project

---
+++ Spotify-Stats_Code/src/main/java/com/example/spotifystats_code/SpotifyStatsCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotifystats_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyStatsCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyStatsCodeApplication.class, args);
+    }
+
+}
---
+++ Spotify-Stats_Code/src/test/java/com/example/spotifystats_code/SpotifyStatsCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotifystats_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyStatsCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
